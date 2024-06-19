a73a1c59c8001b44717f9feb13c325dd50d423b7
preyai <preyaizman@gmail.com>
2024-06-19 18:26:42 +0300

    domophones search

--- src/views/DomophonesPage.vue
+++ src/views/DomophonesPage.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
 
-import {IonContent, IonHeader, IonList, IonPage, IonTitle, IonToolbar} from "@ionic/vue";
-import {onMounted, ref} from "vue";
+import {IonContent, IonHeader, IonList, IonPage, IonSearchbar, IonTitle, IonToolbar} from "@ionic/vue";
+import {computed, onMounted, ref} from "vue";
 import {Domophone, getDomophones} from "@/api/domophones";
-import HouseItem from "@/components/HouseItem.vue";
 import DomophoneItem from "@/components/DomophoneItem.vue";
 
 const domophones = ref<Domophone[]>([])
+const query = ref("")
+const filteredDomophones = computed(() => {
+  if (!query.value)
+    return domophones.value
+  return domophones.value.filter(domophone => domophone.name.toLowerCase().match(query.value.toLowerCase()))
+})
 
 onMounted(() => {
   getDomophones()
@@ -21,10 +26,13 @@ onMounted(() => {
       <IonToolbar>
         <IonTitle>Domophones</IonTitle>
       </IonToolbar>
+      <IonToolbar>
+        <IonSearchbar v-model="query"/>
+      </IonToolbar>
     </IonHeader>
     <IonContent>
       <IonList>
-        <DomophoneItem v-for="item in domophones" :key="item.domophoneId" :domophone="item"/>
+        <DomophoneItem v-for="item in filteredDomophones" :key="item.domophoneId" :domophone="item"/>
       </IonList>
     </IonContent>
   </IonPage>
